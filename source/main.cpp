6b35831b979351f5f067fd68c0f8633d2b844c09
imdever <fdsfsf.afsdf@bk.ru>
2022-02-17 17:09:47 +0400

    Updated spawnPed and load screen patch

--- source/main.cpp
+++ source/main.cpp
@@ -15,12 +15,30 @@ using namespace plugin;
 
 MultiplayerPlugin main_plugin; // create main plugin
 
+#define ADDR_BYPASS_VIDS_USA10					0x747483
+#define ADDR_ENTRY								0xC8D4C0
+
+
+void ViPr(DWORD addr, int size)
+{
+	DWORD d;
+	VirtualProtect((PVOID)addr,size,PAGE_EXECUTE_READWRITE,&d);
+}
+
 MultiplayerPlugin::MultiplayerPlugin(): handle(NULL){
+
+    ViPr(ADDR_BYPASS_VIDS_USA10,6); // OFF load screen
+    *(BYTE *)ADDR_ENTRY = 5;
+    memset((PVOID)ADDR_BYPASS_VIDS_USA10,0x90,6);
+    ViPr(0x866CD8,10);
+    ViPr(0x866CCC,10);
+    strcpy((PCHAR)0x866CD8,"title");
+    strcpy((PCHAR)0x866CCC,"title");
     static int keyPressTime = 0;
     Events::gameProcessEvent += [this] {
     if (FindPlayerPed() && KeyPressed(VK_F9) && CTimer::m_snTimeInMilliseconds - keyPressTime > 500) {
         if (player == nullptr) {
-            AbstractConnection* connection = AbstractConnection::connect("95.47.167.113", uint16_t(7777));
+            AbstractConnection* connection = AbstractConnection::connect("192.168.0.11", uint16_t(7777));
             player = new PlayerClient(connection);
             player->startReading();
         }
@@ -40,14 +58,15 @@ void MultiplayerPlugin::output(string s) {
 
 CPed* MultiplayerPlugin::spawnPed() {
 #ifdef GTASA
-    int modelID = pedModelIds[rand() % 250];
+    int modelID = 0;
     CStreaming::RequestModel(modelID, 0);
     CStreaming::LoadAllRequestedModels(false);
-    CPed* ped = new CCivilianPed(CPopulation::IsFemale(modelID) ? PED_TYPE_CIVFEMALE : PED_TYPE_CIVMALE, modelID);
+    CPed* ped = new CCivilianPed(PED_TYPE_CIVMALE, modelID); // only mans, only cJ.
     if (ped) {
         ped->SetPosn(CVector(2495.330078f, -1676.422485f, 13.337957));
         ped->SetOrientation(0.0f, 0.0f, 0.0f);
         CWorld::Add(ped);
+        ped->m_nCreatedBy = 2; // Don't delete PED!!!!
         ped->PositionAnyPedOutOfCollision();
     }
     return ped;
@@ -56,6 +75,7 @@ CPed* MultiplayerPlugin::spawnPed() {
 #endif
 }
 
+/*
 int pedModelIds[] = { 0, 7, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 32, 33, 34, 35, 36, 37, 43, 44, 45, 46,
     47, 48, 49, 50, 51, 52, 57, 58, 59, 60, 61, 62, 66, 67, 68, 70, 71, 72, 73, 78, 79, 80, 81, 82, 83, 84, 94, 95, 96, 97, 98, 99, 100, 101,
     102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 120, 121, 122, 123, 124, 125, 126, 127, 128, 132,
@@ -64,5 +84,5 @@ int pedModelIds[] = { 0, 7, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 
     221, 222, 223, 227, 228, 229, 230, 234, 235, 236, 239, 240, 241, 242, 247, 248, 249, 250, 252, 253, 254, 255, 258, 259, 260, 261, 262,
     9, 10, 11, 12, 13, 31, 38, 39, 40, 41, 53, 54, 55, 56, 63, 64, 69, 75, 76, 77, 85, 87, 88, 89, 90, 91, 92, 93, 129, 130, 131, 138, 139,
     140, 141, 145, 148, 150, 151, 152, 157, 169, 172, 178, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 201, 205, 207, 211, 214, 215,
-    216, 218, 219, 224, 225, 226, 231, 232, 233, 237, 238, 243, 244, 245, 246, 251, 256, 257, 263 };
+    216, 218, 219, 224, 225, 226, 231, 232, 233, 237, 238, 243, 244, 245, 246, 251, 256, 257, 263 };*/
 
